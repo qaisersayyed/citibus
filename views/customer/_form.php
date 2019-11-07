cc0d5af0dc7b5b353a45a2021644e1b69e6274f2
deepu-07 <deepraj4348@gmail.com>
2019-11-07 11:06:17 +0530

    singup ui updated

--- views/customer/_form.php
+++ views/customer/_form.php
@@ -8,32 +8,95 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-    <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'user_id')->hiddenInput()->label(false); ?>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+<!DOCTYPE html>
+<html lang="en">
 
-    <?= $form->field($model, 'phone_no')->textInput() ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script>
+        function myFunction() {
 
-    <?= $form->field($model, 'email_id')->textInput(['maxlength' => true]) ?>
+            document.getElementById("button").click();
+        }
+    </script>
 
-    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+</head>
 
-    <?= $form->field($model, 'password_repeat')->passwordInput(['maxlength' => true]) ?>
+<body onload="myFunction()" >
+    <div class="site-login">
 
-    <?= $form->field($model, 'e_wallet')->textInput() ?>
 
-    <?= $form->field($model, 'created_at')->hiddenInput()->label(false); ?>
 
-    <?= $form->field($model, 'updated_at')->hiddenInput()->label(false); ?>
+        <div class="container">
 
-    <?= $form->field($model, 'deleted_at')->hiddenInput()->label(false); ?>
+            <button type="button" style="visibility: hidden;;" class="btn btn-primary" data-toggle="modal" data-target="#myModal" id="button">
+                Login
+            </button>
 
-    <div class="form-group">
-        <?= Html::submitButton('Sign Up', ['class' => 'btn btn-success']) ?>
+            <!-- The Modal -->
+            <div class="modal fade" id="myModal">
+                <div class="modal-dialog " style="margin-top:10%">
+                    <div class="modal-content">
+
+                        <!-- Modal Header -->
+                        <div class="modal-header" style="background-color:lightblue;">
+                            <center>
+                                <h4 class="modal-title ">Sign Up <button type="button" class="close" data-dismiss="modal">&times;</button></h4>
+                            </center>
+
+                        </div>
+
+                        <!-- Modal body -->
+                        <div class="modal-body" style="margin-left:15%;margin-right:15%">
+
+                            <?php $form = ActiveForm::begin(); ?>
+
+                            <?= $form->field($model, 'user_id')->hiddenInput()->label(false); ?>
+
+                            <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+                            <?= $form->field($model, 'phone_no')->textInput() ?>
+
+                            <?= $form->field($model, 'email_id')->textInput(['maxlength' => true]) ?>
+
+                            <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+
+                            <?= $form->field($model, 'password_repeat')->passwordInput(['maxlength' => true]) ?>
+
+                            <?= $form->field($model, 'e_wallet')->textInput() ?>
+
+                            <?= $form->field($model, 'created_at')->hiddenInput()->label(false); ?>
+
+                            <?= $form->field($model, 'updated_at')->hiddenInput()->label(false); ?>
+
+                            <?= $form->field($model, 'deleted_at')->hiddenInput()->label(false); ?>
+
+
+
+                            <div class="form-group">
+                                <center><?= Html::submitButton('Sign Up', ['class' => 'btn btn-info']) ?></center>
+                            </div>
+                        </div>
+
+                        <!-- Modal footer -->
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <?php ActiveForm::end(); ?>
+
+
     </div>
-
-    <?php ActiveForm::end(); ?>
-
-</div>
+</body>
+</html>
--- views/site/login.php
+++ views/site/login.php
@@ -25,13 +25,16 @@ $this->title = 'Login';
     </script>
    
 </head>
-<body onload="myFunction()">
+<body onload="myFunction()" style="background: url('') no-repeat center center;" >
 <div class="site-login">
    
 
     <?php $form = ActiveForm::begin([
         'id' => 'login-form',
         'layout' => 'horizontal',
+        'options'=>[
+            
+        ],
         'fieldConfig' => [
             
         ],
@@ -47,13 +50,13 @@ $this->title = 'Login';
 
         <!-- The Modal -->
         <div class="modal fade" id="myModal">
-            <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-dialog " style="margin-top:15%">
                 <div class="modal-content">
 
                     <!-- Modal Header -->
-                    <div class="modal-header" style="background-color:lightblue">
-                        <h4 class="modal-title " style="margin-left:auto">Login</h4>
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <div class="modal-header" style="background-color:lightblue;" >
+                        <center><h4 class="modal-title ">Login  <button type="button" class="close" data-dismiss="modal">&times;</button></h4></center>
+                        
                     </div>
 
                     <!-- Modal body -->
@@ -66,8 +69,8 @@ $this->title = 'Login';
 
 
                         <div class="form-group">
-                            <div style="margin-left:auto">
-                                <?= Html::submitButton('Login', ['class' => 'btn btn-info', 'name' => 'login-button']) ?>
+                            <div >
+                                <center><?= Html::submitButton('Login', ['class' => 'btn btn-info', 'name' => 'login-button']) ?></center>
                             </div>
                         </div>
                     </div>
