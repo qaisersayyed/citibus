9de921536d599761d70345861d7bb1531ee851c0
qaisersayyed <sayyedqaiser03@gmail.com>
2019-11-07 13:22:51 +0530

    seat selection UI changed

--- views/bus-route/bus_view.php
+++ views/bus-route/bus_view.php
@@ -32,7 +32,7 @@ $foundroute = null;
 foreach ($routes as $route) {
     // echo "routeid = ",$route->route_id,"<br>";
     $routesStopType = RouteStopTypeSearch::find()->where(['route_id' => $route->route_id])->all();
-
+     
     foreach ($routesStopType as $rst) {
         if ($rst->stop_id == $from_name->stop_id || $rst->stop_id == $to_name->stop_id) {
             //echo "rst = ",$rst->route_stop_type_id,"<br>";
@@ -47,10 +47,10 @@ foreach ($routes as $route) {
         // echo "count 2 arr fornd = ",count($array), "<br>";
         $frst = RouteStopTypeSearch::find()->where(['stop_id' => $from_name->stop_id, 'route_id' => $route->route_id])->one();
         $trst = RouteStopTypeSearch::find()->where(['stop_id' => $to_name->stop_id, 'route_id' => $route->route_id])->one();
-
+    
         if ($frst->stop_order < $trst->stop_order) {
             //  echo "U","<br>";
-
+     
             $foundroute = $frst->route_id;
             $found_rst = $trst;
         } else {
@@ -72,7 +72,7 @@ if ($foundroute == "") {
 
 
 
-$data = BusRoute::find()->where(['route_id' => $foundroute])->all();
+$data = BusRoute::find()->where(['route_id' => $foundroute ])->all();
 
 //echo $data;
 ?>
@@ -99,9 +99,9 @@ $data = BusRoute::find()->where(['route_id' => $foundroute])->all();
         $busno =  $col->bus->license_plate;
         $from = $col->route->from;
         $to = $col->route->to;
-        $time = $col->timing;
-        $seats_count = Tickets::find()->where(['bus_route_id' => $col->bus_route_id])->count();
-        $bus_seat_count = Bus::find()->where(['bus_id' => $col->bus_id])->one();
+        $time= $col->timing;
+        $seats_count = Tickets::find()->where(['bus_route_id' => $col->bus_route_id ])->count();
+        $bus_seat_count = Bus::find()->where(['bus_id' => $col->bus_id ])->one();
         $seats_left = $bus_seat_count->no_of_seats - $seats_count ?>
         
         
@@ -128,15 +128,6 @@ $data = BusRoute::find()->where(['route_id' => $foundroute])->all();
                               </div>
 
                             </div>
-                        </div>
-
-                        <div class="col-md-2" style="padding-left:160px">
-                            <img src="http://localhost/citibus/web/seat.png" alt="seat-left" height="40px" width="40px">
-
-                        </div>
-                        <div class="col-md-2">
-                            <h4><?php echo $seats_left; ?> Left</h4>
-                        </div>
 
                                 <div class="panel-body">
                                     <div class="row" style="padding-left:20px">
@@ -168,44 +159,20 @@ $data = BusRoute::find()->where(['route_id' => $foundroute])->all();
                                 
                         </div>         
                     </div>
-                </div>
-
-                <div class="panel-body">
-                    <div class="row" style="padding-left:20px">
-                        <div class="col-sm-4">
-                            <p>From</p>
-                            <h4>Margao</h4>
-                        </div>
-                        <div class="col-sm-4">
-                            <p>To</p>
-                            <h4>Panjim</h4>
-                        </div>
-                        <div class="col-sm-2">
-                            <p>Timing</p>
-                            <h4>10:00</h4>
-                        </div>
-                        <div style="padding-top:10px" class="col-sm-2">
-                            <?= Html::a(
-            'Select seats',
-            ['bus-seats/seatselect', 'rst_id' => $found_rst->route_stop_type_id, 'route_id' => $col->route_id, 'bus_id' => $col->bus_id, 'bus_route_id' => $col->bus_route_id],
-            ['class' => 'btn btn-primary']
-        ); ?>
-                        </div>
-                    </div>
-
-
-                </div>
-
-            </div>
-        </div>
-        <br>
+                    <br>
 
     <?php
     }
     ?>
+   
+        </div>
 
-    </div>
+        </body>
+        </html>
+       
+       
 
-</body>
 
-</html>
+
+
+
