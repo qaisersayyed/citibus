6f70f74df448e722d5f855b3aecff1b91d514a29
rahulnath5 <rahulsnath610@gmail.com>
2019-11-08 15:55:05 +0530

    Ticket UI modified

--- views/tickets/viewtickets.php
+++ views/tickets/viewtickets.php
@@ -37,11 +37,11 @@ $dateObject = new DateTime($time);
 
     <div class="responsive" style="height:300px;width:100%;border-radius: 20px;">
             
-        <div class="card text-left" style="background-color:#5F9EA0;height:70px;border-radius: 20px 20px 0px 0px;color:white">
-            <img alt="CitiBus" src="http://localhost/citibus/web/logos/logo-white.png" style="color:#F4B41A; display:inline-blocl;height:60px; width:auto; padding-top:5px; padding-bottom:5px; padding-left:30px">
-            <h2 style="display:inline-block;margin-left:20px">CitiBus</h2>
+        <div class="card text-left" style="background-color:#143D59;height:70px;border-radius: 20px 20px 0px 0px;color:white">
+            <img alt="CitiBus" src="http://localhost/citibus/web/logos/bus-logo.png" style=" display:inline-block;height:62px; width:auto; padding-top:0; padding-bottom:9px; padding-left:30px">
+            <h2 style="display:inline-block;margin-left:20px">Bus Ticket</h2>
         </div>
-        <div class="card-body"  style="border:1px solid #5F9EA0;background: linear-gradient( #ecedef 26%,  #ecedef 100%);">
+        <div class="card-body"  style="border:1px solid black;background-color:#F4B41A; padding-top:15px; padding-bottom:auto; padding-left:10px; padding-right:auto">
              <div class="row" >
                 <div class="col-sm-3" style="float: left;margin-left:50px;display:inline-block">
                     <h4 >Passenger Name: <br><h3><b><?php echo $name?></h3></h4><br>
@@ -54,18 +54,18 @@ $dateObject = new DateTime($time);
                     <h4>Time: <br> <h3><b><?php echo $dateObject->format('h:i A')?></h3></h4><br>
                     
                 </div>
-                <div class="col-sm-3" style="display:inline-block">
+                <div class="col-sm-3" style="display:inline-block;">
                     <h4 >Seat: <br><h3><b><?php echo json_encode($seat)?></h3></h4><br>
                     <h4>Amount: <br> <h3><b><?php echo $amount?></h3></h4><br>
                    
                 </div>
-                <div class="col-sm-3" style="display:inline-block;vertical-align:top;border-left: .18em dashed #fff;">
-                   <img src="http://localhost/citibus/web/uploads/qr.png" alt="" height="150" width="150">
+                <div class="col-sm-2" style="display:inline-block;margin-top:7%;vertical-align:top;border-left: .22em dashed #fff;">
+                   <img src="http://localhost/citibus/web/uploads/qr.png" alt="" height="150" width="auto">
                     <?// echo Html::img('@web/uploads/qr.png', ['width'=>'150px']);?>
                 </div>
             </div>
         </div>
-        <div class="card-footer text-muted" style="background-color:#5F9EA0;height:30px;border-radius: 0px 0px 20px 20px;color:white">
+        <div class="card-footer text-muted" style="background-color:#143D59;height:30px;border-radius: 0px 0px 20px 20px;color:white">
            
          
         </div>
@@ -75,7 +75,7 @@ $dateObject = new DateTime($time);
     
 
     </div class="responsive">
-            <button type="button" style="background-color:#5F9EA0;margin-top:50px;float:right" class="btn btn-primary">Print</button>
+            <button type="button" style="background-color:#143D59;margin-top:50px;float:right" class="btn btn-primary">Print</button>
     </div>
 </div>
 
